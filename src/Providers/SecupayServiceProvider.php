01b2e727fa69b35f16ab7f473a651d2a9ffff076
Edgaras <edgaras.tkacenka@wallee.com>
2024-08-28 16:09:33 +0300

    update cron

--- src/Providers/SecupayServiceProvider.php
+++ src/Providers/SecupayServiceProvider.php
@@ -106,7 +106,7 @@ class SecupayServiceProvider extends ServiceProvider
 
         $secupayServiceProviderHelper->addExecutePaymentContentEventListener();
 
-        $cronContainer->add(CronContainer::EVERY_MINUTE, WebhookCronHandler::class);
+        $cronContainer->add('* * * * *', WebhookCronHandler::class);
     }
 
     private function registerPaymentMethod($payContainer, $id, $class)
